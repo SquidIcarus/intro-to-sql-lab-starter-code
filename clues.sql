7b936620082bff5d20581a492e2a3b39a7fa86f0
User Name <landonisaiah@gmail.com>
2025-09-24 02:10:53 -0700

    solved clue 7

--- clues.sql
+++ clues.sql
@@ -40,3 +40,5 @@ SELECT name FROM cities WHERE id = (SELECT capital FROM countries WHERE code = '
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
+
+SELECT name, countrycode, population FROM cities WHERE population = 91084;
